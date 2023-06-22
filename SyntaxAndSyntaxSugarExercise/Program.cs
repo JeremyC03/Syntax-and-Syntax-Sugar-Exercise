a5b573cbaf243890704c9c351714dfa34f77823e
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-06-22 14:39:59 -0400

    Finished Exercise 1

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Original Code
+            /*int answer = 4;
+            string response;
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + " is greater than or equal to nine";
+            }*/
+
+            var answer = 4;
+            var response = (answer < 9) ? " is less than nine" : " is greater than or equal to nine";
+
+            Console.WriteLine($"{answer}{response}");
         }
     }
 }
